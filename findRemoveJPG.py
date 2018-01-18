bef74bfa728c948af3c1e6f6400da67efe4f79c1
Paul Hor <32339811+paulh@users.noreply.github.com>
2018-01-18 09:57:47 -0800

    function works on single folder.

--- findRemoveJPG.py
+++ findRemoveJPG.py
@@ -1,9 +1,13 @@
 import os
 
-dir_name = 'c:/Users/Paul/Desktop/130_0603 - Copy/'
-dir_list = os.listdir(dir_name)
+def findremovejpg(path):
 
-for files in dir_list:
-    if files.endswith('JPG') and files[:-3] + 'DNG' in dir_list:
-        os.remove(dir_name + files)
-        print(files, 'deleted')
+    dir_name = path
+    dir_list = os.listdir(dir_name)
+
+    for files in dir_list:
+        if files.endswith('JPG') and files[:-3] + 'DNG' in dir_list:
+            os.remove(dir_name + files)
+            print(files, 'deleted')
+
+#findremovejpg('c:/Users/Paul/Desktop/test1/')
